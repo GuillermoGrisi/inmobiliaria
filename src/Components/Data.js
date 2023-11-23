b521c7f8bd3fae878b5c5e1284a75937daef2a9f
Deividds1 <deivid1421@gmail.com>
2023-11-23 11:17:47 -0300

    informacion adicional en data.js

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -1,11 +1,21 @@
 import casa1 from "../assets/casa1.jpg";
 import casa2 from "../assets/casa2.jpg";
 import casa3 from "../assets/casa3.jpg";
+import casa4 from "../assets/casa4.jpg";
+import casa5 from "../assets/casa5.jpg";
+import casa6 from "../assets/casa6.jpg";
+import casa7 from "../assets/casa7.jpg";
+import casa8 from "../assets/casa8.jpg";
+import casa9 from "../assets/casa9.jpg";
+import casa10 from "../assets/casa10.jpg";
+import terreno from "../assets/terreno.jpg";
+import casa12 from "../assets/casa12.jpg";
 
 
 const Data = [
     {
         id: 1,
+        descripcion: "A estrenar en lagomar",
         transaccion: "Venta",
         tipo_de_propiedad: "Casa",
         zona: "Lagomar",
@@ -18,6 +28,7 @@ const Data = [
 
     {
         id: 2,
+        descripcion: "Chalet con hermosa piscina y apto al fondo",
         transaccion: "Venta",
         tipo_de_propiedad: "Casa",
         zona: "Solymar",
@@ -29,6 +40,7 @@ const Data = [
     },
     {
         id: 3,
+        descripcion: "Ideal instituto, residencial o empresas",
         transaccion: "Venta",
         tipo_de_propiedad: "Casa",
         zona: "Shangrila",
@@ -39,17 +51,21 @@ const Data = [
         foto: casa3,
     },
     {
+        id: 4,
+        descripcion: "En zona agreste de el pinar sur, posible 2da. casa",
         transaccion: "Alquiler",
         tipo_de_propiedad: "Apartamento",
-        zona: "Malvin",
+        zona: "El pinar sur",
         dormitorios: "3",
         baños: "2",
         precio: "U$ 30.000",
         mts2: "95 mts2",
-        foto: casa1,
-        },
-    
-        {
+        foto: casa4,
+    },
+
+    {
+        id: 5,
+        descripcion: "Chalet con apartamento al fondo",
         transaccion: "Alquiler",
         tipo_de_propiedad: "Casa",
         zona: "Pocitos",
@@ -57,9 +73,11 @@ const Data = [
         baños: "3",
         precio: "U$ 40.000",
         mts2: "100 mts2",
-        foto: casa2,
-        },
-        {
+        foto: casa5,
+    },
+    {
+        id: 6,
+        descripcion: "Chalet clásico en hermosa zona",
         transaccion: "Venta",
         tipo_de_propiedad: "Casa",
         zona: "Punta Gorda",
@@ -67,70 +85,82 @@ const Data = [
         baños: "2",
         precio: "U$S 400.000",
         mts2: "135 mts2",
-        foto: casa3,
-        },
-        {
-            transaccion: "Venta",
-            tipo_de_propiedad: "Apartamento",
-            zona: "Barra de Carrasco",
-            dormitorios: "2",
-            baños: "2",
-            precio: "U$S 150.000",
-            mts2: "85 mts2",
-            foto: casa1,
-            },
-        
-            {
-            transaccion: "Alquiler",
-            tipo_de_propiedad: "Casa",
-            zona: "Solymar",
-            dormitorios: "3",
-            baños: "2",
-            precio: "U$ 35.000",
-            mts2: "95 mts2",
-            foto: casa2,
-            },
-            {
-            transaccion: "Alquiler",
-            tipo_de_propiedad: "Apartamento",
-            zona: "Parque Batlle",
-            dormitorios: "2",
-            baños: "2",
-            precio: "U$ 32.000",
-            mts2: "90 mts2",
-            foto: casa3,
-            },
-            {
-                transaccion: "Venta",
-                tipo_de_propiedad: "Casa",
-                zona: "Lagomar",
-                dormitorios: "4",
-                baños: "3",
-                precio: "U$S 250.000",
-                mts2: "140 mts2",
-                foto: casa1,
-                },
-            
-                {
-                transaccion: "Alquiler",
-                tipo_de_propiedad: "Casa",
-                zona: "Malvin",
-                dormitorios: "3",
-                baños: "2",
-                precio: "U$ 40.000",
-                mts2: "120 mts2",
-                foto: casa2,
-                },
-                {
-                transaccion: "Venta",
-                tipo_de_propiedad: "Apartamento",
-                zona: "Parque Batlle",
-                dormitorios: "2",
-                baños: "1",
-                precio: "U$S 110.000",
-                mts2: "80 mts2",
-                foto: casa3,
-                },
+        foto: casa6,
+    },
+    {
+        id: 7,
+        descripcion: "Hermoso chalet americano frente a la playa",
+        transaccion: "Venta",
+        tipo_de_propiedad: "Apartamento",
+        zona: "Barra de Carrasco",
+        dormitorios: "2",
+        baños: "2",
+        precio: "U$S 150.000",
+        mts2: "85 mts2",
+        foto: casa7,
+    },
+
+    {
+        id: 8,
+        descripcion: "Moderna y confortable propiedad en punta pinares",
+        transaccion: "Alquiler",
+        tipo_de_propiedad: "Casa",
+        zona: "Punta pinares",
+        dormitorios: "3",
+        baños: "2",
+        precio: "U$ 35.000",
+        mts2: "95 mts2",
+        foto: casa8,
+    },
+    {
+        id: 9,
+        descripcion: "Gran propiedad en hermosa ubicación del lado sur",
+        transaccion: "Alquiler",
+        tipo_de_propiedad: "Apartamento",
+        zona: "Parque Batlle",
+        dormitorios: "2",
+        baños: "2",
+        precio: "U$ 32.000",
+        mts2: "90 mts2",
+        foto: casa9,
+    },
+    {
+        id: 10,
+        descripcion: "Chalet en una planta, con gran parque y piscina",
+        transaccion: "Venta",
+        tipo_de_propiedad: "Casa",
+        zona: "Lagomar",
+        dormitorios: "4",
+        baños: "3",
+        precio: "U$S 250.000",
+        mts2: "140 mts2",
+        foto: casa10,
+    },
+
+    {
+        id: 11,
+        descripcion: "Terreno para construir",
+        transaccion: "Alquiler",
+        tipo_de_propiedad: "Casa",
+        zona: "Malvin",
+        dormitorios: "3",
+        baños: "2",
+        precio: "U$ 40.000",
+        mts2: "120 mts2",
+        foto: terreno,
+    },
+    {
+        id: 12,
+        descripcion: "Chalet estilo americano",
+        transaccion: "Venta",
+        tipo_de_propiedad: "Apartamento",
+        zona: "Parque Batlle",
+        dormitorios: "2",
+        baños: "1",
+        precio: "U$S 110.000",
+        mts2: "80 mts2",
+        foto: casa12,
+    },
 ]
 
 export default Data;
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -4,36 +4,39 @@ import { CiHeart } from "react-icons/ci";
 import data from './Data'
 
 function Search() {
- /*  const [showDropdown, setShowDropdown] = useState(false);
-  const [selectedOption, setSelectedOption] = useState("");
-  const [showPriceDropdown, setShowPriceDropdown] = useState(false);
-  const [selectedPriceOption, setSelectedPriceOption] = useState("");
   const [estatesList, setEstatesList] = useState(data);
   const [searchValue, setSearchValue] = useState("");
-
-  const toggleDropdown = () => {
-    setShowDropdown(!showDropdown);
-  };
-
-  const handleOptionClick = (option) => {
-    setSelectedOption(option);
-    setShowDropdown(false);
-  };
-  const togglePriceDropdown = () => {
-    setShowPriceDropdown(!showPriceDropdown);
-  };
-
-  const handlePriceOptionClick = (priceOptions) => {
-    setSelectedPriceOption(priceOptions);
-    setShowPriceDropdown(false);
-  };
+  /*  const [showDropdown, setShowDropdown] = useState(false);
+   const [selectedOption, setSelectedOption] = useState("");
+   const [showPriceDropdown, setShowPriceDropdown] = useState(false);
+   const [selectedPriceOption, setSelectedPriceOption] = useState("");
+   
+ 
+   const toggleDropdown = () => {
+     setShowDropdown(!showDropdown);
+   };
+ 
+   const handleOptionClick = (option) => {
+     setSelectedOption(option);
+     setShowDropdown(false);
+   };
+   const togglePriceDropdown = () => {
+     setShowPriceDropdown(!showPriceDropdown);
+   };
+ 
+   const handlePriceOptionClick = (priceOptions) => {
+     setSelectedPriceOption(priceOptions);
+     setShowPriceDropdown(false);
+   };
+ 
+   
+ 
+   const options = ["Ventas", "Alquiler"];
+   const priceOptions = ["U$S", "$U"]; */
 
   const filteredEstateList = estatesList.filter((estate) =>
     estate.tipo_de_propiedad.toLowerCase().includes(searchValue.toLowerCase())
   );
-
-  const options = ["Ventas", "Alquiler"];
-  const priceOptions = ["U$S", "$U"]; */
 
   return (
     <div className='searchContainer'>
@@ -57,16 +60,9 @@ function Search() {
                 ))}
               </ul>
             </div>
-<<<<<<< HEAD
-          )}</div>
-
-        <div>
-          <input
-=======
           )}</div> */}
-          {/* <div>
+        {/* <div>
            <input
->>>>>>> 09b8606386247ecfcb40b999cb3d2a38249ab962
             type="text"
             name="priceInput"
             id="priceInput"
@@ -85,8 +81,15 @@ function Search() {
               </ul>
             </div>
           )}
-<<<<<<< HEAD
-        </div>
+          </div> */}
+        <select id="transaccionSelect">
+          <option value="ventas" selected>Ventas</option>
+          <option value="alquileres">Alquileres</option>
+        </select>
+        <select id="tipoMonedaSelect">
+          <option value="U$S" selected>U$S</option>
+          <option value="$U">$U</option>
+        </select>
         <input
           type="text"
           name="PrMinInput"
@@ -97,89 +100,41 @@ function Search() {
           name="PrMaxInput"
           id="PrMaxInput"
           placeholder="Precio máximo" />
-        <select
-          type="text"
-          name="tipoPropInput"
-          id="tipoPropInput"
-          placeholder="Tipo de propiedad" />
+        <select id="tipoPropSelect">
+          <option value="" disabled selected hidden>Tipo de propiedad</option>
+          <option value="casas">Casas</option>
+          <option value="apartamentos">Apartamentos</option>
+          <option value="terrenos">Terrenos</option>
+        </select>
         <input
           type="text"
           name="zonaInput"
           id="zonaInput"
           placeholder="Zonas" />
-        <input
-          type="text"
-          name="habInput"
-          id="habInput"
-          placeholder="Habitaciones" />
-        <input
-          type="text"
-          name="bañoInput"
-          id="bañoInput"
-          placeholder="Baños" />
+        <select id="habSelect">
+          <option value="" disabled selected hidden>Habitaciones</option>
+          <option value="monoambiebte">Monoambiente</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5omas">5 o más</option>
+        </select>
+        <select id="bañoSelect">
+          <option value="" disabled selected hidden>Baños</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5omas">5 o más</option>
+        </select>
         <input
           type="text"
           name="refInput"
           id="refInput"
           placeholder="Referencia" />
-        <button>Buscar</button>
-        <button><CiHeart className='heart' /></button>
-=======
-          </div> */}
-          <select id="transaccionSelect">
-             <option value="ventas" selected>Ventas</option>
-             <option value="alquileres">Alquileres</option>
-        </select>
-        <select id="tipoMonedaSelect">
-             <option value="U$S" selected>U$S</option>
-             <option value="$U">$U</option>
-        </select>
-          <input
-            type="text"
-            name="PrMinInput"
-            id="PrMinInput"
-            placeholder="Precio mínimo"/>
-            <input
-            type="text"
-            name="PrMaxInput"
-            id="PrMaxInput"
-            placeholder="Precio máximo"/>
-        <select id="tipoPropSelect">
-             <option value="" disabled selected hidden>Tipo de propiedad</option>
-             <option value="casas">Casas</option>
-             <option value="apartamentos">Apartamentos</option>
-             <option value="terrenos">Terrenos</option>
-        </select>
-             <input
-            type="text"
-            name="zonaInput"
-            id="zonaInput"
-            placeholder="Zonas"/>
-            <select id="habSelect">
-             <option value="" disabled selected hidden>Habitaciones</option>
-             <option value="monoambiebte">Monoambiente</option>
-             <option value="1">1</option>
-             <option value="2">2</option>
-             <option value="3">3</option>
-             <option value="4">4</option>
-             <option value="5omas">5 o más</option>
-        </select>
-        <select id="bañoSelect">
-             <option value="" disabled selected hidden>Baños</option>
-             <option value="1">1</option>
-             <option value="2">2</option>
-             <option value="3">3</option>
-             <option value="4">4</option>
-             <option value="5omas">5 o más</option>
-        </select>
-            <input
-            type="text"
-            name="refInput"
-            id="refInput"
-            placeholder="Referencia"/>
-            <button className='buscarButton'>Buscar</button>
-            <button className='heartButton'><CiHeart className='heart'/></button>
->>>>>>> 09b8606386247ecfcb40b999cb3d2a38249ab962
+        <button className='buscarButton'>Buscar</button>
+        <button className='heartButton'><CiHeart className='heart' /></button>
       </form>
       <div className='estates-list-container'>
         {filteredEstateList.map((estate) => (
